b3d0e77a26866d1abafbaa8039793e8549905c9f
Giang03 <89713275+giang03@users.noreply.github.com>
2022-09-09 12:17:31 +0700

    lam

---
+++ indongchu.cpp
@@ -0,0 +1,11 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << pow(n, 2) << " " << pow(n, 3) << pow(n,4);
+}
---
+++ sobethuk.cpp
@@ -0,0 +1,14 @@
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,k; cin >> n >> k;
+    int a[n];
+    for(int &x : a) cin >> x;
+    sort(a,a+n);
+    cout << a[k] ;
+    // return 0;
+}
